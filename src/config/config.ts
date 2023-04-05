e2cba093c8d91dd73f3fe3df54adf8424506a70f
Roaring20sNFT <roaring20snft@gmail.com>
2023-04-05 13:58:14 -0600

    updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,23 +21,10 @@ Here are two examples:
 
 const unlockables: Unlockable[] = [
   {
-    cid: "bafybeiehgpaip4f7jafzf7imgannx3nnv3ubaiwp6ph56mlyzijpqxi45m", // HTML blog example
+    cid: "bafybeigenoc2pygi5eri54rosy3oio6iptwwzetlglnvawbl6lwmf3ziri", // R3020s Pvt. Hamilton
     nftId: [
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' to unlock this.
-  },
-  {
-    cid: "bafybeihx5eacyxeydcpvudwxa242rnjhn67femy46gzas5d2djb24ti5mi", // MP4 video example
-    nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-    ], // You need to own 'GM #2' to unlock this.
-  },
-  {
-    cid: "bafybeihhx5v3saq3b7n55ub5q3atuw2udbqc5ictkv2ih7vd3hxptu22nu", // Web game example
-    nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' & 'GM #2' to unlock this.
+      "0xa8c46eef48f67be1de7573ae839fe0edd1c49199ca7bdefb64e20eba77ed46b9",
+    ], // You need to own 'R3020s Pvt. Hamilton NFT' to unlock this.
   },
 ];
 
